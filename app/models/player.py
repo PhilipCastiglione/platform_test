1a1f50d1fc161afee202385e4ca691fce91773ed
Philip Castiglione <philipcastiglione@gmail.com>
2019-10-01 20:01:59 +1000

    player model, (first) migration

---
+++ app/models/player.py
@@ -0,0 +1,22 @@
+from app import db
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(length=20), unique=True)
+    gold = db.Column(db.Integer())
+    attack_strength = db.Column(db.Integer())
+    hit_points = db.Column(db.Integer())
+    luck = db.Column(db.Integer())
+
+    def __repr__(self):
+        return '<Player {}>'.format(self.id)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gold": self.gold,
+            "attack_strength": self.attack_strength,
+            "hit_points": self.hit_points,
+            "luck": self.luck
+        }
